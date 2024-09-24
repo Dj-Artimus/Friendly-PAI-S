11d39847ecce486a3efbdd5438c2c58b09fc2266
Dj-Artimus-X <Pratik.djArtimus@gmail.com>
2024-09-25 02:40:01 +0530

    Some changes

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -4,7 +4,7 @@ const generateTokenAndSetCookie = (res, user) => {
     const token = jwt.sign({user}, process.env.JWT_SECRET, { expiresIn: `30d` });
     res.cookie( "token", token, { 
         httpOnly: true, 
-        sameSite: "strict",  // Set to "none" if your site requires cross-site access
+        sameSite: "none",  // Set to "none" if your site requires cross-site access
         secure: process.env.NODE_ENV === "production",  // Set to true if your site requires HTTPS
         maxAge: 24 * 60 * 60 * 1000 * 30   // 30 days
     });
